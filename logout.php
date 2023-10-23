d3adeca5801e44b98529a4f6e1da49908b7c92a0
Daniel Garza <danielrgarza12@gmail.com>
2023-10-22 23:22:18 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,19 +2,25 @@
     //Logout file for nav bar. Will kill session and send to main page
     session_start();
     include 'database.php';
+    $redirectTo = 'index.php'; // Default redirect location
 
-     // Check if the user is logged in
-     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+
+     // checks if the user logged in is an employee
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && isset($_SESSION['user']['Employee_ID'])) {
         $Employee_ID = $_SESSION['user']['Employee_ID'];
 
         // mark employee as clocked out
         $mysqli->query("UPDATE employee SET clocked_in=0 WHERE Employee_ID='$Employee_ID'");
+
+        //send to employee login
+        $redirectTo = 'employee_login.php';
 
         //close connection
         $mysqli->close();
     }
 
     session_destroy(); // This will clear the session
-    header("Location: index.php"); // Redirecting to index.php
+    header("Location: $redirectTo"); // Redirecting to appropriate page
     exit();
+
 ?>
