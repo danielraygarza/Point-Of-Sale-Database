6f2a7b04290216799f193a06d9bbd15965ca4b37
Daniel Garza <danielrgarza12@gmail.com>
2023-10-23 21:46:49 -0500

    Update employee_home.php

--- employee_home.php
+++ employee_home.php
@@ -27,11 +27,11 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
             <a href="#">Profile</a> -->
             <!-- if user is logged in, logout button will display -->
             <?php
-            if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-                echo '<a href="logout.php">Logout</a>';
-            }
             if ($_SESSION['user']['Title_Role'] == 'MAN' && $_SERVER['REQUEST_URI'] != '/reports.php') {
                 echo '<a href="reports.php">Reports</a>';
+            }
+            if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+                echo '<a href="logout.php">Logout</a>';
             }
             ?>
         </div>
