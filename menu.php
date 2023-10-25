3d415c99009d0a6b3c4ddf1fe8091e6e844ce6e7
Bryant Huynh <bryant.huynh03@gmail.com>
2023-10-25 17:56:14 -0500

    Test Commit
    Test

--- menu.php
+++ menu.php
@@ -1,13 +1,13 @@
 <?php
+    include 'database.php'
     session_start(); //continues current session to keep user logged in
-    //dont have to be logged in to access this page so guest can still order
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
     <title>POS Pizza</title>
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="menu.css">
     <link rel="icon" href="img/pizza.ico" type="image/x-icon">
 </head>
 <body>
@@ -16,14 +16,39 @@
         <a href="menu.php">Order now</a>
         <!-- <a href="#">Profile</a> -->
         <?php
-            // in nav bar, shows log out or log in
             if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
                 echo '<a href="logout.php">Logout</a>';
-            } else {
-                echo '<a href="customer_login.php">Log in</a>';
             }
         ?>
     </div>
+
+    <p>TEST</p>
+
+    <?php 
+        $sql = "SELECT * FROM pizza";
+        $result = mysqli->query($sql);
+        $resultCheck = mysqli_num_rows($result);
+    
+        while ($row = mysqli.fetch_assoc($result)) {
+    ?>
+
+    <main>
+        <div class = "card">
+            <div class = "image">
+                <img src= <?php echo $row["Image_Path"]; ?> alt="">
+            </div>
+            <p class = "pizza_name"><?php echo $row["Name"]; ?></p>
+            <p class = "description"><?php echo $row["Description"]; ?></p>
+            <p class = "calories"><?php echo $row["Calories"]; ?> cals</p>
+            <p class = "price"><b>$<?php echo $row["Cost"]; ?></b></p>
+            <div class = "add-to-cart">ADD TO ORDER</div>
+        </div>
+    </main>
+
+    <?php 
+        }
+    ?>
+    
 
     <form action="" method="post">
             <h2>Menu</h2>
