32ea2811d34e2a5d2744330d9cc2c288fe671753
Bryant Huynh <bryant.huynh03@gmail.com>
2023-10-26 18:34:09 -0500

    Menu Reading from DB
    Menu can now read from the DB

--- menu.php
+++ menu.php
@@ -1,6 +1,10 @@
 <?php
     include 'database.php';
-    session_start(); //continues current session to keep user logged in
+    session_start();
+
+    $sql = "SELECT * FROM pizza;";
+    $result = $mysqli->query($sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -23,23 +27,24 @@
         ?>
     </div>
 
-    
+    <?php 
+        while($row = mysqli_fetch_assoc($result)) {
+    ?>
 
     <main>
-        <div class = "topGap">
-            <div class = "card">
-                <div class = "image">
-                    <img src= "img/cheese_pizza.jpeg" alt="">
-                </div>
-                <p class = "pizza_name">Pizza Name</p>
-                <p class = "description">TEST</p>
-                <p class = "calories">1200 cals</p>
-                <p class = "price"><b>$12.99</b></p>
-                <div class = "add-to-cart">ADD TO ORDER</div>
+        <div class = "card">
+            <div class = "image">
+                <img src= <?php echo $row["Image_Path"]; ?> alt="">
             </div>
+            <p class = "pizza_name"><?php echo $row["Name"]; ?></p>
+            <p class = "description"><?php echo $row["Description"]; ?></p>
+            <p class = "calories"><?php echo $row["Calories"]; ?> cals</p>
+            <p class = "price"><b>$<?php echo $row["Cost"]; ?></b></p>
+            <div class = "add-to-cart">ADD TO ORDER</div>
         </div>
     </main>
     
+    <?php } ?>
 
     <form action="" method="post">
             <h2>Menu</h2>
