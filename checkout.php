d90c2789c108b3b0eb98b3eea1e2d75b3394967a
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-10-29 23:02:53 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -63,7 +63,7 @@ function getCartItemCount() {
 
                         if ($row) {
                          $toppingPrice = $row['Price'];
-                            echo "$toppingName:                    $toppingPrice";
+                            echo "$toppingName:"                    "$row['Price']";
                             } else {
                               echo "Topping not found.";
                                       }
