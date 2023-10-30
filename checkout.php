bb02b78c97ec6f4e165afaa58ec322fc79e3b7fc
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-10-30 15:45:41 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -42,6 +42,7 @@ function getCartItemCount() {
 <div class="checkout-window">
     <form action="" method="post">
         <h2>Shopping Cart</h2>
+        <div class="cart-panel">
             <ul class="cart-items">
                 <?php
                 // Assuming you have a cart stored in a session or database
