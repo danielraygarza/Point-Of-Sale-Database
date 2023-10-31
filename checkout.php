f895901308b022750649be0bbc9e029b526b3cf2
Daniel Garza <danielrgarza12@gmail.com>
2023-10-30 20:51:16 -0500

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -78,7 +78,7 @@ function getCartItemCount()
                     ?>
                 </ul>
             </div>
-            <a href="index.php" type="submit" class="button">Place Order</a>
+            <a href="checkout.php" type="submit" class="button">Place Order</a>
         </div>
     </form>
         <?php
