1b14d18210856d74c692c6972a09c07f64164d6f
Daniel Garza <danielrgarza12@gmail.com>
2023-10-30 23:24:40 -0500

    getemployee function

--- reports.php
+++ reports.php
@@ -66,6 +66,26 @@
                 <label for="employeeDropdown">Select Employee:</label>
                 <select name="employeeDropdown" id="employeeDropdown">
                 <?php
+                    function getEmployeeData() {
+                        include_once("./database.php"); 
+                        $sql = "SELECT `Employee_ID`, `E_First_Name`, `E_Last_Name` FROM `employee`";
+                        $result = mysqli_query($mysqli, $sql);
+                
+                        if (!$result) {
+                            die("Error: " . mysqli_error($connection));
+                        }
+                
+                        $employeeData = array();
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $employeeData[] = [
+                                'Employee_ID' => $row['Employee_ID'],
+                                'Name' => $row['E_First_Name'] . ' ' . $row['E_Last_Name'],
+                            ];
+                        }
+                        mysqli_free_result($result);
+                        return $employeeData;
+                    }
+                    
                     include_once("./include/function/getEmplyee.php");
                     $employeeData = getEmployeeData();
 
