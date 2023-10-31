d4060b47a83c63a9b0b9c72923e7dd4f2c8081ee
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-10-30 19:28:44 -0500

    LETS FUCKING GOOOO

--- checkout.php
+++ checkout.php
@@ -42,7 +42,7 @@ function getCartItemCount() {
     
     <form action="" method="post">
     <div class="checkout-window">
-    <h3 class="cart-heading">Pizza Cart</h3>
+    <h2 class="cart-heading">Pizza Cart</h2>
         <div class="cart-panel">
             <ul class="cart-items">
                 <?php
