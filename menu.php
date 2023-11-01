e74812f458be60c676501d4cfa04634da9799956
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-11-01 13:15:53 -0500

    Update menu.php
    make add to cart work

--- menu.php
+++ menu.php
@@ -5,12 +5,13 @@
         $_SESSION['cart'] = [];
     }
    
-
     $sql = "SELECT * FROM pizza;";
     $result = $mysqli->query($sql);
+
     function addToCart($itemId) {
         $_SESSION['cart'][] = $itemId;
     }
+
     function getCartItemCount() {
         return count($_SESSION['cart']);
     }
@@ -34,11 +35,11 @@
                 echo '<a href="logout.php">Logout</a>';
             }
         ?>
-         <a href="checkout.php" id="cart-button">Cart (<?php echo getCartItemCount(); ?>)</a>
+        <a href="checkout.php" id="cart-button">Cart (<?php echo getCartItemCount(); ?>)</a>
     </div>
     
     <form action="" method="post">
-            <h2>Menu</h2>  
+        <h2>Menu</h2>  
     </form>
 
     <main>
@@ -46,25 +47,32 @@
             while($row = mysqli_fetch_assoc($result)) {
         ?>
             <div class="card">
-    <div class="image">
-        <img src=<?php echo $row["Image_Path"]; ?> alt="">
-    </div>
-    <p class="pizza_name"><?php echo $row["Name"]; ?> (<?php echo $row["Size_Option"]; ?>)</p>
-    <p class="description"><?php echo $row["Description"]; ?></p>
-    <p class="calories"><?php echo $row["Calories"]; ?> cals</p>
-    <p class="price"><b>$<?php echo $row["Cost"]; ?></b></p>
-    <?php
-    if ($row["Is_Pizza"] == 1) {
-        echo '<div class="customize"><a href="customize_pizza.php">CUSTOMIZE</a></div>';
-    } else {
-        echo '<div class="add-to-cart"><button onclick="addToCart(' . $row["Pizza_ID"] . ')">Add to Cart</button></div>';
-    }
-    ?>
-</div>
+                <div class="image">
+                    <img src=<?php echo $row["Image_Path"]; ?> alt="">
+                </div>
+                <p class="pizza_name"><?php echo $row["Name"]; ?> (<?php echo $row["Size_Option"]; ?>)</p>
+                <p class="description"><?php echo $row["Description"]; ?></p>
+                <p class="calories"><?php echo $row["Calories"]; ?> cals</p>
+                <p class="price"><b>$<?php echo $row["Cost"]; ?></b></p>
+                <?php
+                if ($row["Is_Pizza"] == 1) {
+                    echo '<div class="customize"><a href="customize_pizza.php">CUSTOMIZE</a></div>';
+                } else {
+                    echo '<div class="add-to-cart"><button onclick="addToCart(' . $row["Pizza_ID"] . ')">Add to Cart</button></div>';
+                }
+                ?>
+            </div>
         <?php } ?>
     </main>
     
-
-
+    <!-- JavaScript function to add items to the cart -->
+    <script>
+        function addToCart(pizzaID) {
+            // Add the item to the cart array in the session
+            <?php
+            echo "$_SESSION['cart'][] = pizzaID;";
+            ?>
+        }
+    </script>
 </body>
 </html>
