876fd10079a785df8df56322eabded7ec2ec46b7
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-11-01 16:12:53 -0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -38,7 +38,7 @@ function getEmployeeData()
 // Get a list of Store Ids from Pizza_Store table as array $storeIdData
 function getStoreID(){
     include_once("./database.php");
-    $sql = "SELECT 'Pizza_Store_ID' FROM 'pizza_store'";
+    $sql = "SELECT `Pizza_Store_ID` FROM `pizza_store`";
     $result = mysqli_query($mysqli, $sql);
 
     if(!$result){
@@ -99,7 +99,14 @@ function getStoreID(){
         <div id="storeSelection" style="display: none;">
             <label for="storeDropdown">Select Store:</label>
             <select name="storeDropdown" id="storeDropdown">
-                <option value="test">Default</option>
+                <!-- <option value="test">Default</option> -->
+                <?php
+                $storeIdData = getStoreID();
+                foreach ($storeIdData as $store){
+                    $storeId = $store['Pizza_Store_ID'];
+                    echo "<option value='$storeId'>$storeId</option>";
+                }
+                ?>
             </select>
 
         </div><br>
