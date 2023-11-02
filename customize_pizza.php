2a56e9ccd8b8e9a1fa351fd7fa98f8dcda323f2a
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-11-02 15:09:27 -0500

    add to cart script

--- customize_pizza.php
+++ customize_pizza.php
@@ -29,7 +29,6 @@
     <div class="navbar">
         <a href="index.php">Home</a>
         <a href="menu.php">Order now</a>
-        <!-- <a href="#">Profile</a> -->
         <?php
             if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
                 echo '<a href="logout.php">Logout</a>';
@@ -39,21 +38,22 @@
     </div>
 
     <main>
-            <div class = "card">
-                <div class = "top-card">
-                    <div class = "right-top">
-                        <div class = "image">
-                            <img src= "img/cheese_pizza.jpeg" alt="">
-                        </div>
-                    </div>
-                    <div class = "left-top">
-                        <p class = "pizza_name">Customize Your Pizza</p>
-                        <p class = "description">Select any of the following toppings for your pizza:</p>
-                        <p class = "calories">Somewhere between 3 and 9871 Calories</p>
+        <div class="card">
+            <div class="top-card">
+                <div class="right-top">
+                    <div class="image">
+                        <img src="img/cheese_pizza.jpeg" alt="">
                     </div>
                 </div>
-                <p class = "price"><b>Calculated Price: AUTO UPDATE TOTAL PRICE</b></p>
-                <div class="toppings-list">
+                <div class="left-top">
+                    <p class="pizza_name">Customize Your Pizza</p>
+                    <p class="description">Select any of the following toppings for your pizza:</p>
+                    <p class="calories">Somewhere between 3 and 9871 Calories</p>
+                </div>
+            </div>
+            <p class="price"><b>Calculated Price: AUTO UPDATE TOTAL PRICE</b></p>
+            <div class="toppings-list">
+                <form id="toppingForm">
                     <?php
                     while ($toppingRow = $toppingsResult->fetch_assoc()) {
                         $toppingName = $toppingRow['topping_name'];
@@ -61,11 +61,28 @@
                         echo '<label><input type="checkbox" name="toppings[]" value="' . $toppingName . '">' . $toppingName . ' - $' . $toppingPrice . '</label><br>';
                     }
                     ?>
-                </div>
+                </form>
+                <button id="addToCartButton" onclick="addSelectedToppingsToCart()">Add to Cart</button>
             </div>
+        </div>
     </main>
-    
 
+    <script>
+        function addSelectedToppingsToCart() {
+            // Get the form and collect checked checkboxes
+            const form = document.getElementById('toppingForm');
+            const checkboxes = form.querySelectorAll('input[name="toppings[]"]:checked');
+            
+            // Extract the values (topping names) from the checked checkboxes
+            const selectedToppings = Array.from(checkboxes).map(checkbox => checkbox.value);
 
+            // Add the selected toppings to the cart using the addToCart function
+            selectedToppings.forEach(topping => addToCart(topping));
+            
+            // Optionally, you can update the cart count or show a message
+            const cartItemCount = getCartItemCount();
+            alert(selectedToppings.length + ' topping(s) added to the cart. Total items in the cart: ' + cartItemCount);
+        }
+    </script>
 </body>
 </html>
