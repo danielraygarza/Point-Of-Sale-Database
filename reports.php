5c19548a35bcb7afdeeba891597313d013377cad
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-11-02 11:42:38 -0500

    Update reports.php

--- reports.php
+++ reports.php
@@ -130,7 +130,7 @@ function getStoreID(){
                     $stores = $mysqli->query("SELECT * FROM pizza_store");
                     if ($stores->num_rows > 0) {
                         while($row = $stores->fetch_assoc()) {
-                            if ($row["Pizza_Store_ID"] != 1) { continue; } //only shows store ID 1. can delete to show all
+                            //if ($row["Pizza_Store_ID"] != 1) { continue; } //only shows store ID 1. can delete to show all
                             echo '<option value="' . $row["Pizza_Store_ID"] . '" ' . $selected . '>' . $row["Store_Address"] . ' - ' . $row["Store_City"] . '</option>';
                         }
                     }
