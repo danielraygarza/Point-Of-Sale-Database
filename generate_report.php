f943a3da567dd9784a79d053d902c94e998a6e3a
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-11-12 13:29:17 -0600

    Update generate_report.php

--- generate_report.php
+++ generate_report.php
@@ -309,7 +309,7 @@
                         FROM PIZZA_STORE P 
                         LEFT JOIN ORDERS O
                         ON P.Pizza_Store_ID = O.Store_ID
-                        WHERE DATE(O.Date_Of_Order) BETWEEN '$stDate' AND '$endDate'
+                        WHERE P.Pizza_Store_ID = '$storeId' AND DATE(O.Date_Of_Order) BETWEEN '$stDate' AND '$endDate'
                         GROUP BY P.Pizza_Store_ID, P.Store_Address;";
                     }
 
