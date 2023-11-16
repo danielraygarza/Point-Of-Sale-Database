459482f0a6e6291efe2aa8c1b2f51eebc40f9741
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-11-15 22:22:21 -0600

    Update generate_report.php

--- generate_report.php
+++ generate_report.php
@@ -304,7 +304,8 @@
                         LEFT JOIN ORDERS O
                         ON P.Pizza_Store_ID = O.Store_ID
                         WHERE P.Pizza_Store_ID = '$storeId' AND DATE(O.Date_Of_Order) BETWEEN '$stDate' AND '$endDate' 
-                        GROUP BY P.Pizza_Store_ID, P.Store_Address;";
+                        GROUP BY P.Pizza_Store_ID, P.Store_Address)
+                        AS Subquery;";
                         // Get order info for daily orders
                         $ordSql = "SELECT Order_ID, 
                         Date_Of_Order, 
