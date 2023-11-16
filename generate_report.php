416ec164c750356d719d670fda134f91de333302
MalikIbtisam <ibtisam.malik2000@gmail.com>
2023-11-16 06:39:41 -0600

    export

--- generate_report.php
+++ generate_report.php
@@ -629,11 +629,14 @@
                         if ($orderResult) {
                             if (mysqli_num_rows($orderResult) > 0) {
                                 echo '<h2>Order Details</h2>';
+                                echo '<br><button id="export-pdf" class="btn btn-primary">Export to PDF</button>
+<button id="export-csv" class="btn btn-success">Export to CSV</button>
+<br>';
 
                                  // Start scrollable area
                                 echo '<div class="scrollable-area">';
 
-                                echo "<table border='1' class='table_update'>";
+                                echo "<table border='1' class='table_update' id='export-data'>";
                                 echo "<tr>
                                         <th class='th-spacing'>Order ID</th>
                                         <th class='th-spacing'>Date Of Order</th>
@@ -688,6 +691,60 @@
         ?>
 
     </form>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="table2csv.js"></script>
+
+    <script type="text/javascript">
+        $(document).on('click', '#export-csv', function(e) {
+                e.preventDefault();
+                $("#export-data").table2csv({
+                    filename:'export-csv.csv'
+                });
+
+
+
+        });
+
+        $('#export-pdf').on('click', function (e) {
+             e.preventDefault();
+
+             var tableHtml = $('#export-data').html();
+
+             $.post('export_pdf.php', { data: tableHtml }, function (response) {
+                // Convert base64 PDF data to Blob
+                var byteCharacters = atob(response.fileData);
+                var byteArrays = [];
+
+                for (var offset = 0; offset < byteCharacters.length; offset += 512) {
+                    var slice = byteCharacters.slice(offset, offset + 512);
+
+                    var byteNumbers = new Array(slice.length);
+                    for (var i = 0; i < slice.length; i++) {
+                        byteNumbers[i] = slice.charCodeAt(i);
+                    }
+
+                    var byteArray = new Uint8Array(byteNumbers);
+                    byteArrays.push(byteArray);
+                }
+
+                var blob = new Blob(byteArrays, { type: 'application/pdf' });
+
+                // Create a download link and trigger the download
+                var link = document.createElement('a');
+                link.href = URL.createObjectURL(blob);
+                link.download = 'table.pdf';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            }, 'json');
+            
+        });
+
+       
+    </script>
 
 </body>
 
