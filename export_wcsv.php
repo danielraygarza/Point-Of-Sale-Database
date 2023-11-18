a494896a94ead6660af6ef4cc78775d0103216f4
sS373Nn <119764691+sS373Nn@users.noreply.github.com>
2023-11-17 18:27:19 -0600

    Update export_wcsv.php

--- export_wcsv.php
+++ export_wcsv.php
@@ -4,6 +4,9 @@ if (isset($_POST['export'])) {
     // Debug
     echo 'Export button clicked!';
 
+    // Retrieve data
+    $exportData = json_decode($_POST['export_data'], true);
+
     // Set the headers for CSV file
     header('Content-Type: text/csv');
     header('Content-Disposition: attachment; filename="exported_data.csv"');
@@ -12,13 +15,13 @@ if (isset($_POST['export'])) {
     $output = fopen('php://output', 'w');
 
     // Output the column headings dynamically
-    if (!empty($exportArray)) {
-        $firstRow = reset($exportArray);
+    if (!empty($exportData)) {
+        $firstRow = reset($exportData);
         fputcsv($output, array_keys($firstRow));
     }
 
     // Loop through the fetched results and output data
-    foreach ($exportArray as $row) {
+    foreach ($exportData as $row) {
         fputcsv($output, $row);
     }
 
