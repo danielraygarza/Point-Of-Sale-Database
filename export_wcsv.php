0b78a24edc34ee0d8a18923e5d930d20bfbfd5e1
mcfixstuff <147212255+mcfixstuff@users.noreply.github.com>
2023-11-17 20:25:37 -0600

    trying a bug fix from chatGPT

--- export_wcsv.php
+++ export_wcsv.php
@@ -1,52 +1,41 @@
 <?php
-    session_start();
-    include 'database.php'; // Include the database connection details
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+session_start();
+include 'database.php'; // Include the database connection details
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-   // Redirects if not manager/CEO or accessed directly via URL
-    if (!isset($_SESSION['user']['Title_Role']) || ($_SESSION['user']['Title_Role'] !== 'CEO' && $_SESSION['user']['Title_Role'] !== 'MAN')) {
-        header("Location: employee_login.php");
-        exit; // Make sure to exit so that the rest of the script won't execute
+// Redirects if not manager/CEO or accessed directly via URL
+if (!isset($_SESSION['user']['Title_Role']) || ($_SESSION['user']['Title_Role'] !== 'CEO' && $_SESSION['user']['Title_Role'] !== 'MAN')) {
+    header("Location: employee_login.php");
+    exit; // Make sure to exit so that the rest of the script won't execute
+}
+
+if (isset($_POST['export'])) {
+    // Set the headers for CSV file
+    header('Content-Type: text/csv');
+    header('Content-Disposition: attachment; filename="exported_data.csv"');
+
+    // Retrieve data
+    $exportData = json_decode($_POST['export_data'], true);
+
+    // Open a file pointer
+    $output = fopen('php://output', 'w');
+
+    // Output the column headings dynamically
+    if (!empty($exportData)) {
+        $firstRow = reset($exportData);
+        fputcsv($output, array_keys($firstRow));
     }
 
-    if (isset($_POST['export'])) {
-        
-
-        // // Set the headers for CSV file
-        header('Content-Type: text/csv');
-        header('Content-Disposition: attachment; filename="exported_data.csv"');
-
-        // Debug
-        echo 'Export button clicked!<br>';
-
-        // Retrieve data
-        $exportData = json_decode($_POST['export_data'], true);
-
-        // Debug
-        echo 'Exported Data:<br>';
-        print_r($exportData);
-
-        // Open a file pointer
-        $output = fopen('php://output', 'w');
-
-        // Output the column headings dynamically
-        if (!empty($exportData)) {
-            $firstRow = reset($exportData);
-            fputcsv($output, array_keys($firstRow));
-        }
-
-        // Loop through the fetched results and output data
-        foreach ($exportData as $row) {
-            fputcsv($output, $row);
-        }
-
-        // Close the file pointer
-        fclose($output);
-    } else{
-        echo 'Form not submitted';
+    // Loop through the fetched results and output data
+    foreach ($exportData as $row) {
+        fputcsv($output, $row);
     }
+
+    // Close the file pointer
+    fclose($output);
+} else {
+    echo 'Form not submitted';
+}
 ?>
-
-</body>
