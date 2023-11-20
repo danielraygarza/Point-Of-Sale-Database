df992204f3c8323f44ce7476be4faec5fd1050cc
Daniel Garza <danielrgarza12@gmail.com>
2023-11-20 04:22:15 -0600

    Update database.php

--- database.php
+++ database.php
@@ -1,8 +1,13 @@
 <?php
-$host = getenv("DB_HOST");
-$username = getenv("DB_USERNAME");
-$password = getenv("DB_PASSWORD");
-$dbname = getenv("DB_NAME");
+// Hiding credentials doesnt allow you to run the website locally
+$host = "pospizza.mysql.database.azure.com";
+$username = "danielgarza";
+$password = "#drgarza8";
+$dbname = "pos";
+// $host = getenv("DB_HOST");
+// $username = getenv("DB_USER");
+// $password = getenv("DB_PASS");
+// $dbname = getenv("DB_NAME");
 $port = 3306;
 $mysqli = mysqli_init();
 mysqli_ssl_set($mysqli, NULL, NULL, "./DigiCertGlobalRootCA.crt.pem", NULL, NULL);
