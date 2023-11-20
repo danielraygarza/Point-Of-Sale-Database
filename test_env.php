0e30c4db2d9fd48a29f8291d20f960d3b4f49c3d
Daniel Garza <danielrgarza12@gmail.com>
2023-11-20 05:11:55 -0600

    Delete test_env.php

--- test_env.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo 'DB_HOST: ' . getenv('DB_HOST') . "<br>";
-echo 'DB_NAME: ' . getenv('DB_NAME') . "<br>";
-echo 'DB_USERNAME: ' . getenv('DB_USERNAME') . "<br>";
-echo 'DB_PASSWORD: ' . getenv('DB_PASSWORD') . "<br>";
-?>
