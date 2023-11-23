3f3767ec08b50c81403ddc9706c9c4dc943d5cb8
Daniel Garza <danielrgarza12@gmail.com>
2023-11-22 18:07:27 -0600

    Update customer_login.php

--- customer_login.php
+++ customer_login.php
@@ -75,7 +75,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <a href="signup.php" class="button">Sign up</a>
 
         <div>
-            <p><a href="menu.php">Continue as guest</a></p>
+            <p><a href="menu.php?guest=1">Continue as guest</a></p>
         </div>
     </form>
 
